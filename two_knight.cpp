5925353779e3fc6def6c44bd7e51c27274de6c01
arpitverma <arpitv747@gmail.com>
2023-11-08 17:41:36 +0530

    Pb7 Two knight done

---
+++ two_knight.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void print() { cout << "\n"; }
+template <typename T, typename... types>
+void print(const T &first, const types &...args)
+{
+    cout << first << " ";
+    print(args...);
+}
+void debug(int x) { cerr << x; }
+void debug(long x) { cerr << x; }
+void debug(long long x) { cerr << x; }
+void debug(unsigned x) { cerr << x; }
+void debug(unsigned long x) { cerr << x; }
+void debug(unsigned long long x) { cerr << x; }
+void debug(float x) { cerr << x; }
+void debug(double x) { cerr << x; }
+void debug(long double x) { cerr << x; }
+void debug(char x) { cerr << '\'' << x << '\''; }
+void debug(const char *x) { cerr << '\"' << x << '\"'; }
+void debug(const string &x) { cerr << '\"' << x << '\"'; }
+void debug(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void debug(const pair<T, V> &x);
+template <typename T>
+void debug(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), debug(i);
+    cerr << "}";
+}
+template <typename T, typename V>
+void debug(const pair<T, V> &x)
+{
+    cerr << '{';
+    debug(x.first);
+    cerr << ", ";
+    debug(x.second);
+    cerr << '}';
+}
+template <typename ST>
+void debug(stack<ST> st)
+{
+    cerr << " ";
+    while (!st.empty())
+    {
+        cerr << st.top() << " ";
+        st.pop();
+    }
+}
+template <typename Q>
+void debug(queue<Q> que)
+{
+    while (!que.empty())
+    {
+        cerr << que.front() << " ";
+        que.pop();
+    }
+}
+template <typename DQ>
+void debug(deque<DQ> dque)
+{
+    while (!dque.empty())
+    {
+        cerr << dque.front() << " ";
+        dque.pop_front();
+    }
+}
+void dbg() { cerr << "]\n"; }
+template <typename T, typename... V>
+void dbg(T t, V... v)
+{
+    debug(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    dbg(v...);
+}
+
+#ifdef DEBUG
+#define dbg(x...)                                                             \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = ["; \
+    dbg(x);                                                                   \
+    cerr << "\e[39m" << endl;
+#else
+#define dbg(x...) 32
+#endif
+
+const vector<pair<int, int>> d4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const vector<pair<int, int>> d8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+bool comp(pair<long long, long long> a, pair<long long, long long> b)
+{
+    if (a.first > b.first)
+        return true;
+    return false;
+}
+
+void solve()
+{
+/*
+ Approach
+ you have to subtract the case when they attack on each from the total no of case (nc2) where n=k*k;
+ 
+ you obsereved in 2*3 rectangle and 3*2 there are two postion where they attack so count both type rectangle in the matrix and multiply
+ by 2 to count total attacking postion
+ */
+
+long long n ;
+cin >> n;
+for( int i=1 ; i<=n ;i++){
+    long long ans= ((i*i)*(i*i-1))/2; //total possible ways to place two knight
+    if(i>2)
+    ans-=4*(i-1)*(i-2);
+    print(ans);}
+
+
+
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long t = 1;
+    long long n = 0;
+    // cin >> t;
+    // for (int i = 1; i <= t; i++)
+    {
+
+        solve();
+    }
+}
