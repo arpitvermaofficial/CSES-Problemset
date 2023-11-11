d461a8f3d54e80b3c78ee0fe1ed955649126cfc1
arpitverma <arpitv747@gmail.com>
2023-11-12 01:14:16 +0530

    Pb31 trafficn lights Done

---
+++ Sorting_and_Searching/trafic_lights.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void print() { cout << "\n"; }
+template <typename T, typename... types>
+void print(const T &first, const types &...args)
+{
+    cout << first << " ";
+    print(args...);
+}
+void debug(int x) { cerr << x; }
+void debug(long x) { cerr << x; }
+void debug(long long x) { cerr << x; }
+void debug(unsigned x) { cerr << x; }
+void debug(unsigned long x) { cerr << x; }
+void debug(unsigned long long x) { cerr << x; }
+void debug(float x) { cerr << x; }
+void debug(double x) { cerr << x; }
+void debug(long double x) { cerr << x; }
+void debug(char x) { cerr << '\'' << x << '\''; }
+void debug(const char *x) { cerr << '\"' << x << '\"'; }
+void debug(const string &x) { cerr << '\"' << x << '\"'; }
+void debug(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void debug(const pair<T, V> &x);
+template <typename T>
+void debug(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), debug(i);
+    cerr << "}";
+}
+template <typename T, typename V>
+void debug(const pair<T, V> &x)
+{
+    cerr << '{';
+    debug(x.first);
+    cerr << ", ";
+    debug(x.second);
+    cerr << '}';
+}
+template <typename ST>
+void debug(stack<ST> st)
+{
+    cerr << " ";
+    while (!st.empty())
+    {
+        cerr << st.top() << " ";
+        st.pop();
+    }
+}
+template <typename Q>
+void debug(queue<Q> que)
+{
+    while (!que.empty())
+    {
+        cerr << que.front() << " ";
+        que.pop();
+    }
+}
+template <typename DQ>
+void debug(deque<DQ> dque)
+{
+    while (!dque.empty())
+    {
+        cerr << dque.front() << " ";
+        dque.pop_front();
+    }
+}
+void dbg() { cerr << "]\n"; }
+template <typename T, typename... V>
+void dbg(T t, V... v)
+{
+    debug(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    dbg(v...);
+}
+
+#ifdef DEBUG
+#define dbg(x...)                                                              \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << "[" << #x << "] = [";   \
+    dbg(x); 	                                                               \
+    cerr << "\e[39m" << endl;
+#else
+#define dbg(x...) 32
+#endif
+
+const vector<pair<int, int>> d4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const vector<pair<int, int>> d8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+bool comp(pair<long long, long long> a, pair<long long, long long> b)
+{
+    if (a.first > b.first)
+        return true;
+    return false;
+}
+
+void solve()
+{
+    /*
+    Approach
+    we look for lower bound and upper bound of evry no and calculate
+    first dis bw upper boud and lower bound which is max passage till yet 
+    but now this should be remove as traffic light must be place inbetween this
+    so calculate new two passge bw uppr bound and no , no and lower bound
+    add them to list of all possible passge and pick the maximum passage from list
+    */
+   ll n, x;
+   cin >> n >> x;
+   set<ll>s;
+   map<ll ,ll>m;
+   s.insert(0);
+   s.insert(n);
+    m[n]++;
+   for(int i=0; i<x; i++) {
+    ll a;
+    cin >> a;
+   
+    auto it= s.lower_bound(a);
+    int r=*it;
+    int l=*(--it);
+    m[r-l]--;
+    if(!m[r-l]) {
+        m.erase(r-l);
+    }
+    m[r-a]++;
+    m[a-l]++;
+    s.insert(a);
+    print(((--m.end())->first));
+   }
+
+
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long t = 1;
+    long long n = 0;
+    // cin >> t;
+    // for (int i = 1; i <= t; i++)
+    {
+
+        solve();
+    }
+}
