facf61d2caf2557899da3fd0aa380cec41553198
arpitverma <arpitv747@gmail.com>
2023-11-11 09:41:54 +0530

    Pb24 sum of two no done:

---
+++ Sorting_and_Searching/sum_of_two_numbers.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void print() { cout << "\n"; }
+template <typename T, typename... types>
+void print(const T &first, const types &...args)
+{
+    cout << first << " ";
+    print(args...);
+}
+void debug(int x) { cerr << x; }
+void debug(long x) { cerr << x; }
+void debug(long long x) { cerr << x; }
+void debug(unsigned x) { cerr << x; }
+void debug(unsigned long x) { cerr << x; }
+void debug(unsigned long long x) { cerr << x; }
+void debug(float x) { cerr << x; }
+void debug(double x) { cerr << x; }
+void debug(long double x) { cerr << x; }
+void debug(char x) { cerr << '\'' << x << '\''; }
+void debug(const char *x) { cerr << '\"' << x << '\"'; }
+void debug(const string &x) { cerr << '\"' << x << '\"'; }
+void debug(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void debug(const pair<T, V> &x);
+template <typename T>
+void debug(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), debug(i);
+    cerr << "}";
+}
+template <typename T, typename V>
+void debug(const pair<T, V> &x)
+{
+    cerr << '{';
+    debug(x.first);
+    cerr << ", ";
+    debug(x.second);
+    cerr << '}';
+}
+template <typename ST>
+void debug(stack<ST> st)
+{
+    cerr << " ";
+    while (!st.empty())
+    {
+        cerr << st.top() << " ";
+        st.pop();
+    }
+}
+template <typename Q>
+void debug(queue<Q> que)
+{
+    while (!que.empty())
+    {
+        cerr << que.front() << " ";
+        que.pop();
+    }
+}
+template <typename DQ>
+void debug(deque<DQ> dque)
+{
+    while (!dque.empty())
+    {
+        cerr << dque.front() << " ";
+        dque.pop_front();
+    }
+}
+void dbg() { cerr << "]\n"; }
+template <typename T, typename... V>
+void dbg(T t, V... v)
+{
+    debug(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    dbg(v...);
+}
+
+#ifdef DEBUG
+#define dbg(x...)                                                              \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << "[" << #x << "] = [";   \
+    dbg(x); 	                                                               \
+    cerr << "\e[39m" << endl;
+#else
+#define dbg(x...) 32
+#endif
+
+const vector<pair<int, int>> d4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const vector<pair<int, int>> d8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+bool comp(pair<long long, long long> a, pair<long long, long long> b)
+{
+    if (a.first < b.first)
+        return true;
+        
+    return false;
+}
+
+void solve()
+{
+    /*
+    Approach
+    Use two pointer approach
+    Case 1- When sum is of combination of two diff no
+    Sort the array 
+    store all num index in map
+    traverse from start in array then find if any no exist from end 
+    by which sum equal to given sum .if sum exceeds than move second pointer 
+    backward
+    Case 2 - when sum is combination of same no
+    traverse in map and check if 2*no ==x then print both index
+    of no
+
+    */
+    long long n,x;
+cin >> n >> x;
+vector<ll >v(n);
+map<ll ,vector<ll>>m;
+
+for(int i=0; i<n; i++) {
+    cin >> v[i];
+    
+    m[v[i]].push_back(i+1);
+ }
+ 
+ sort(v.begin(),v.end());
+ for(int i=0,j=n-1;i<j;i++) {
+    while(v[i]+v[j]>x) {
+        j--;
+    }
+    if( v[i]+v[j]==x && i<j && v[i]!=v[j]) {
+        
+        print(m[v[i]][0], m[v[j]][0]);
+      
+        return;
+    }
+ }
+ for( auto i :m) {
+    if(2*i.first==x &&m[i.first].size()>1) {
+        print(m[i.first][0], m[i.first][1]);
+        return;
+    }
+  }
+ 
+
+ print("IMPOSSIBLE");
+
+
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long t = 1;
+    long long n = 0;
+    // cin >> t;
+    // for (int i = 1; i <= t; i++)
+    {
+
+        solve();
+    }
+}   
