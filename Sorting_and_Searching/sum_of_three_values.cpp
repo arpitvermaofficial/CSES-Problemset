a21382c9dcea3838f78035541c96b78e449f9add
arpitverma <arpitv747@gmail.com>
2023-11-13 23:05:53 +0530

    Pb40 sum of two number  done

---
+++ Sorting_and_Searching/sum_of_three_values.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void print() { cout << "\n"; }
+template <typename T, typename... types>
+void print(const T &first, const types &...args)
+{
+    cout << first << " ";
+    print(args...);
+}
+void debug(int x) { cerr << x; }
+void debug(long x) { cerr << x; }
+void debug(long long x) { cerr << x; }
+void debug(unsigned x) { cerr << x; }
+void debug(unsigned long x) { cerr << x; }
+void debug(unsigned long long x) { cerr << x; }
+void debug(float x) { cerr << x; }
+void debug(double x) { cerr << x; }
+void debug(long double x) { cerr << x; }
+void debug(char x) { cerr << '\'' << x << '\''; }
+void debug(const char *x) { cerr << '\"' << x << '\"'; }
+void debug(const string &x) { cerr << '\"' << x << '\"'; }
+void debug(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void debug(const pair<T, V> &x);
+template <typename T>
+void debug(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), debug(i);
+    cerr << "}";
+}
+template <typename T, typename V>
+void debug(const pair<T, V> &x)
+{
+    cerr << '{';
+    debug(x.first);
+    cerr << ", ";
+    debug(x.second);
+    cerr << '}';
+}
+template <typename ST>
+void debug(stack<ST> st)
+{
+    cerr << " ";
+    while (!st.empty())
+    {
+        cerr << st.top() << " ";
+        st.pop();
+    }
+}
+template <typename Q>
+void debug(queue<Q> que)
+{
+    while (!que.empty())
+    {
+        cerr << que.front() << " ";
+        que.pop();
+    }
+}
+template <typename DQ>
+void debug(deque<DQ> dque)
+{
+    while (!dque.empty())
+    {
+        cerr << dque.front() << " ";
+        dque.pop_front();
+    }
+}
+void dbg() { cerr << "]\n"; }
+template <typename T, typename... V>
+void dbg(T t, V... v)
+{
+    debug(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    dbg(v...);
+}
+
+#ifdef DEBUG
+#define dbg(x...)                                                              \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << "[" << #x << "] = [";   \
+    dbg(x); 	                                                               \
+    cerr << "\e[39m" << endl;
+#else
+#define dbg(x...) 32
+#endif
+
+const vector<pair<int, int>> d4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const vector<pair<int, int>> d8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+bool comp(pair<long long, long long> a, pair<long long, long long> b)
+{
+    if (a.first > b.first)
+        return true;
+    return false;
+}
+
+void solve()
+{
+    /*
+    Approach
+    We use two pointer approach
+    by brute force we have to use three loop which exceed time complexity
+    and for second inner loop third inner loop run approx n times
+    but by two pointer approach on second loop and third loop 
+    it only iterates n which reduce complexity n2 to n
+    */
+    ll n ,x;
+    cin >> n >> x;
+    vector<pair<ll,ll>>v(n);
+    for(int i=0; i<n; i++) {
+        ll y;
+        cin >> y;
+        v[i]={y,i+1};
+       
+    }
+    
+    sort(v.begin(), v.end());
+    for( int i=0; i<n; i++ ) {
+        ll x2=x-v[i].first;
+        for(ll j=i+1, k=n-1; j<k; j++) {
+            while(j<k && v[j].first+v[k].first>x2) {
+                k--;
+            }
+            if(j<k&&v[j].first+v[k].first==x2) {
+                cout << v[i].second << " " << v[j].second << " " << v[k].second;
+                return;
+            }
+        }
+            
+   
+    }
+     print("IMPOSSIBLE");
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long t = 1;
+    long long n = 0;
+    // cin >> t;
+    // for (int i = 1; i <= t; i++)
+    {
+
+        solve();
+    }
+}
