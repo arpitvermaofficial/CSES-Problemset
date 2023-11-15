82984f0907aafa88d5ced4bb494af9b6cb6b75ad
arpitverma <arpitv747@gmail.com>
2023-11-15 17:06:32 +0530

    Pb46 subarray distinct values done

---
+++ Sorting_and_Searching/subarray_distinct_values.cpp
@@ -0,0 +1,192 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void print() { cout << "\n"; }
+template <typename T, typename... types>
+void print(const T &first, const types &...args)
+{
+    cout << first << " ";
+    print(args...);
+}
+void debug(int x) { cerr << x; }
+void debug(long x) { cerr << x; }
+void debug(long long x) { cerr << x; }
+void debug(unsigned x) { cerr << x; }
+void debug(unsigned long x) { cerr << x; }
+void debug(unsigned long long x) { cerr << x; }
+void debug(float x) { cerr << x; }
+void debug(double x) { cerr << x; }
+void debug(long double x) { cerr << x; }
+void debug(char x) { cerr << '\'' << x << '\''; }
+void debug(const char *x) { cerr << '\"' << x << '\"'; }
+void debug(const string &x) { cerr << '\"' << x << '\"'; }
+void debug(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void debug(const pair<T, V> &x);
+template <typename T>
+void debug(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), debug(i);
+    cerr << "}";
+}
+template <typename T, typename V>
+void debug(const pair<T, V> &x)
+{
+    cerr << '{';
+    debug(x.first);
+    cerr << ", ";
+    debug(x.second);
+    cerr << '}';
+}
+template <typename ST>
+void debug(stack<ST> st)
+{
+    cerr << " ";
+    while (!st.empty())
+    {
+        cerr << st.top() << " ";
+        st.pop();
+    }
+}
+template <typename Q>
+void debug(queue<Q> que)
+{
+    while (!que.empty())
+    {
+        cerr << que.front() << " ";
+        que.pop();
+    }
+}
+template <typename DQ>
+void debug(deque<DQ> dque)
+{
+    while (!dque.empty())
+    {
+        cerr << dque.front() << " ";
+        dque.pop_front();
+    }
+}
+void dbg() { cerr << "]\n"; }
+template <typename T, typename... V>
+void dbg(T t, V... v)
+{
+    debug(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    dbg(v...);
+}
+
+#ifdef DEBUG
+#define dbg(x...)                                                            \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << "[" << #x << "] = ["; \
+    dbg(x);                                                                  \
+    cerr << "\e[39m" << endl;
+#else
+#define dbg(x...) 32
+#endif
+
+const vector<pair<int, int>> d4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const vector<pair<int, int>> d8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+bool comp(pair<long long, long long> a, pair<long long, long long> b)
+{
+    if (a.first > b.first)
+        return true;
+    return false;
+}
+
+void solve()
+{
+    /*
+    Approach
+    We use two pointer with hashmap
+    in two pointer approach no of subarray bw i and j is distance
+    bw i and j
+    in this solution we move i till m have distict elements equal to k
+    and then move pointer j till map size is less than k
+    and count the no of subarray by i-j formula
+    */
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    ll ans = 0;
+    ll i = -1, j = -1;
+    map<ll, ll> m;
+
+    while (true)
+    {
+        while (i < n)
+        {
+            i++;
+            if (i < n)
+            {
+                m[v[i]]++;
+
+                if (m.size() <= k)
+                {
+                    ans += i - j;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        if (i == n && m.size() <= k)
+        {
+            break;
+        }
+
+        while (j < i)
+        {
+            j++;
+            if (m[v[j]])
+            {
+                m[v[j]]--;
+                if (m[v[j]] <= 0)
+                {
+                    m.erase(v[j]);
+                }
+            }
+            else
+            {
+
+                m[v[j]]++;
+            }
+            if (m.size() > k)
+            {
+                continue;
+            }
+            else
+            {
+                ans += i - j;
+                break;
+            }
+        }
+    }
+    print(ans);
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long t = 1;
+    long long n = 0;
+    // cin >> t;
+    // for (int i = 1; i <= t; i++)
+    {
+
+        solve();
+    }
+}
