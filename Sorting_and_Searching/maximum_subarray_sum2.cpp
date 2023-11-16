10e433b2970a25e06dc26db7a8e4f4dcbf2c283c
arpitverma <arpitv747@gmail.com>
2023-11-16 23:40:12 +0530

    Pb50 Maximum Subarray Sum2  done

---
+++ Sorting_and_Searching/maximum_subarray_sum2.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void print() { cout << "\n"; }
+template <typename T, typename... types>
+void print(const T &first, const types &...args)
+{
+    cout << first << " ";
+    print(args...);
+}
+void debug(int x) { cerr << x; }
+void debug(long x) { cerr << x; }
+void debug(long long x) { cerr << x; }
+void debug(unsigned x) { cerr << x; }
+void debug(unsigned long x) { cerr << x; }
+void debug(unsigned long long x) { cerr << x; }
+void debug(float x) { cerr << x; }
+void debug(double x) { cerr << x; }
+void debug(long double x) { cerr << x; }
+void debug(char x) { cerr << '\'' << x << '\''; }
+void debug(const char *x) { cerr << '\"' << x << '\"'; }
+void debug(const string &x) { cerr << '\"' << x << '\"'; }
+void debug(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void debug(const pair<T, V> &x);
+template <typename T>
+void debug(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), debug(i);
+    cerr << "}";
+}
+template <typename T, typename V>
+void debug(const pair<T, V> &x)
+{
+    cerr << '{';
+    debug(x.first);
+    cerr << ", ";
+    debug(x.second);
+    cerr << '}';
+}
+template <typename ST>
+void debug(stack<ST> st)
+{
+    cerr << " ";
+    while (!st.empty())
+    {
+        cerr << st.top() << " ";
+        st.pop();
+    }
+}
+template <typename Q>
+void debug(queue<Q> que)
+{
+    while (!que.empty())
+    {
+        cerr << que.front() << " ";
+        que.pop();
+    }
+}
+template <typename DQ>
+void debug(deque<DQ> dque)
+{
+    while (!dque.empty())
+    {
+        cerr << dque.front() << " ";
+        dque.pop_front();
+    }
+}
+void dbg() { cerr << "]\n"; }
+template <typename T, typename... V>
+void dbg(T t, V... v)
+{
+    debug(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    dbg(v...);
+}
+
+#ifdef DEBUG
+#define dbg(x...)                                                              \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << "[" << #x << "] = [";   \
+    dbg(x); 	                                                               \
+    cerr << "\e[39m" << endl;
+#else
+#define dbg(x...) 32
+#endif
+
+const vector<pair<int, int>> d4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const vector<pair<int, int>> d8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+bool comp(pair<long long, long long> a, pair<long long, long long> b)
+{
+    if (a.first > b.first)
+        return true;
+    return false;
+}
+
+void solve()
+{
+    /*
+    Approach
+    this question in done in n3 if use brute force
+    to optimize the solution
+    after observing you notice that every time we have to only focus on max of element bw rnage a to b
+    which can be find in log n using set and to calculate sum of array length of a or b we use prefix
+    array which give maximum sum of array length from a to b
+    */
+    ll n, a, b;
+    cin >> n >> a >> b;
+    vector<ll>v(n+1,0) ;
+    for(int i=1; i<=n; i++) {
+        cin >> v[i];
+        v[i]+=v[i-1];
+    }
+        set<pair<ll, ll >>s;
+        for(int i=a; i<=b; i++) {
+            s.insert({v[i], i});
+        }
+        ll maxsum=-1e18;
+        for(int i=1; i<=n-a+1; i++) {
+            maxsum=max(maxsum, s.rbegin()->first-v[i-1]);
+            s.erase({v[i+a-1],i+a-1});
+            if(i+b <=n ) {
+                s.insert({v[i+b],i+b});
+            }
+        }
+        print(maxsum);
+
+
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long t = 1;
+    long long n = 0;
+    // cin >> t;
+    // for (int i = 1; i <= t; i++)
+    {
+
+        solve();
+    }
+}
